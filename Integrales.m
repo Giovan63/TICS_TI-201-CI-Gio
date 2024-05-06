0c980aa7a1f3e888d60f2a5e6dcbc3f71358ac06
Giovanni <giovannirosa466@gmail.com>
2024-05-06 10:47:33 -0600

    Se agrego el problema 13 de la tarea 01

--- Integrales.m
+++ Integrales.m
@@ -11,5 +11,8 @@ p1 = int(fx,x)
 p1_fx = 3*x^2 +4*x +1
 i_fx = int(p1_fx, x)
 
-fx = x^2 - 2x
+fx = x^2 - 2*x
 ifx = int (fx, x, 3, 6)
+
+p13_fx = sin(2*x)
+p13_i_fx = double (int (p13_fx, x, 0, 1.5707 ))
